7d51de1460f800344686507cfa01960a358b0e30
Michael Zechner <michael-zechner@gmx.at>
2021-10-11 12:53:28 +0200

    content component created +hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeroComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
